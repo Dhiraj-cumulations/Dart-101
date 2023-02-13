84afedb3aa84d442d49007913f77648633c337dd
dhiraj-cumulations <dhiraj.dn@gmail.com>
2023-02-13 16:27:13 +0530

    extension changed

---
+++ Classes.dart
@@ -0,0 +1,16 @@
+void main(){
+  print('Hello Dayrt!');
+  int a= 2;
+  String c= "Hello";
+  double d= 2.25;
+  var m= 8788;
+  var listData= [1,2,3,5];
+  print(listData);
+  sample(a);
+}
+
+int sample(int b){
+  //int a=4;
+  print(b);
+  return 6;
+}
